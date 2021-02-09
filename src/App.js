726a552b172e7d1dbfb439eaf760e5bc5661bc87
raj vaya <rajkumarvaya@gmail.com>
2021-02-09 21:18:22 +0530

    Local Storage Support Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Form from "./components/Form";
 import ToDoList from "./components/ToDoList";
+import userEvent from "@testing-library/user-event";
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -9,8 +10,15 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [filteredtodos, setFilteredTodos] = useState([]);
 
+  //this effect will only run onece to get data from local storage
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  //use effect run everytime when app starts but this one will work whne todos or status changes
   useEffect(() => {
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
 
   const filterHandler = () => {
@@ -24,6 +32,20 @@ function App() {
       default:
         setFilteredTodos(todos);
         break;
+    }
+  };
+
+  ///save to loacl
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let localTodos =  JSON.parse(localStorage.getItem("todos")); 
+      setTodos(localTodos);
     }
   };
 
